a6d88b67fcedec353aebf6809d27e251ddf02576
jukirchn <jkirchner@mail.de>
2022-02-13 21:31:20 +0100

    Added solutions for practice time - part 1

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,11 @@
 // Create two variables, firstName and lastName
+let firstName = "Diana"
+let lastName = "Morgenstern"
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName
 
 // Log fullName to the console
+console.log(fullName)
 
 
--- 2. Practice time - part 1/6. Calculator challenge/index.js
+++ 2. Practice time - part 1/6. Calculator challenge/index.js
@@ -3,10 +3,37 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+
+
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
+sumElement = document.getElementById("sum-el")
+
+function add() {
+    let sum = num1 + num2
+    sumElement.textContent = "Sum: "
+    sumElement.textContent += sum
+}
+
+function subtract() {
+    let minus = num1 - num2
+    sumElement.textContent = "Subtract: "
+    sumElement.textContent += minus
+}
+
+function divide() {
+    let div = num1 / num2
+    sumElement.textContent = "Divide: "
+    sumElement.textContent += div
+}
+
+function multiply() {
+    let product = num1 + num2
+    sumElement.textContent = "Product: "
+    sumElement.textContent += product
+}
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
