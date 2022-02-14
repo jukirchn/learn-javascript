9213e2df7ef1717e30b57fa9419ec99d838af870
jukirchn <jkirchner@mail.de>
2022-02-14 19:55:17 +0100

    added tasks 1 to 13 in 3. Build a Blackjack game

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
+let firstCard = 13
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("You're out of the game!")
+}
